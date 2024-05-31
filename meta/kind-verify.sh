59138c6f095a4b9526bc09728a7b2349c113f78f
Rajat Kumar Vishwakarma <79074817+rajat709@users.noreply.github.com>
2024-05-31 06:46:50 +0530

    Update kind-verify.sh

--- meta/kind-verify.sh
+++ meta/kind-verify.sh
@@ -1,1 +1,37 @@
+#!/bin/bash
 
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Update package lists
+echo "Updating package lists..."
+sudo apt update
+
+# Check if Docker is running
+if ! pgrep -x "docker" >/dev/null; then
+    echo "Docker is not running. Please start Docker."
+    exit 1
+else
+    sudo sed -i '/accept-nvidia-visible-devices-as-volume-mounts/c\accept-nvidia-visible-devices-as-volume-mounts = true' /etc/nvidia-container-runtime/config.toml
+    echo "Docker is running."
+fi
+
+# Check if kind is installed
+if ! command_exists kind; then
+    echo "kind is not installed. Please install kind."
+    exit 1
+else
+    echo "kind is installed."
+fi
+
+# Check if kubectl is installed
+if ! command_exists kubectl; then
+    echo "kubectl is not installed. Please install kubectl."
+    exit 1
+else
+    echo "kubectl is installed."
+fi
+
+echo "Follow up and create your KinD cluster ⚡"
